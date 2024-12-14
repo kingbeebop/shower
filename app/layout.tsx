add887cb8e0fb1db311e83f02aa6970909dab5f2
Brendan Ward <kingbeebop@gmail.com>
2024-12-14 12:46:44 -0500

    add use client to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
+'use client'
+
 import './globals.css';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 
-export const metadata = {
-  title: 'My Next.js App',
-  description: 'A modern Next.js app with Redux Toolkit',
-};
 
 export default function RootLayout({
   children,
