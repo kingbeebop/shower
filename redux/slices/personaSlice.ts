c09504a188b39e45254f1929727904f4f9e442bf
Brendan Ward <kingbeebop@gmail.com>
2024-12-14 16:46:52 -0500

    removed persona removal

--- redux/slices/personaSlice.ts
+++ redux/slices/personaSlice.ts
@@ -4,7 +4,9 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Persona, PersonaState } from '../../types/Persona';
 
 const initialState: PersonaState = {
-  personas: [],
+  personas: [
+
+  ],
 };
 
 const personaSlice = createSlice({
@@ -12,22 +14,16 @@ const personaSlice = createSlice({
   initialState,
   reducers: {
     addPersona(state, action: PayloadAction<Omit<Persona, 'id'>>) {
-      // Get the next available ID (next number after the highest current id)
-      const nextId = state.personas.length ? Math.max(...state.personas.map(p => p.id)) + 1 : 1;
 
       // Create the new persona object with the assigned ID
-      const newPersona = { ...action.payload, id: nextId };
+      const newPersona = { ...action.payload };
       
       // Push the new persona to the personas array
       state.personas.push(newPersona);
-    },
-    removePersona(state, action: PayloadAction<number>) {
-      // Remove persona by id
-      state.personas = state.personas.filter(persona => persona.id !== action.payload);
-    },
+    }
   },
 });
 
-export const { addPersona, removePersona } = personaSlice.actions;
+export const { addPersona } = personaSlice.actions;
 
 export default personaSlice.reducer;
